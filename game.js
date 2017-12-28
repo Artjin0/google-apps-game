b7b23b4f1cface896e83c8f1221a6d6dc0b5af02
Kevin Griffin <coemgen@hotmail.com>
2017-12-28 13:52:26 -0500

    WIP

--- game.js
+++ game.js
@@ -4,15 +4,16 @@ var ballX = 75;
 var ballY = 75;
 var ballSpeedX = 5;
 var ballSpeedY = 7;
-var canvas, canvasContext;
+var canvas;
+var canvasContext;
 window.onload = function () {
     "use strict";
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
+    canvas = document.getElementById("gameCanvas");
+    canvasContext = canvas.getContext("2d");
 
     var fPS = 30;
     setInterval(window.updateAll, 1000 / fPS);
-    canvasContext.fillStyle = 'black';
+    canvasContext.fillStyle = "black";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 };
 
@@ -33,10 +34,10 @@ function updateAll() {
     if (ballY < 0) {
         ballSpeedY *= -1;
     }
-    canvasContext.fillStyle = 'black';
+    canvasContext.fillStyle = "black";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
-    canvasContext.fillStyle = 'white';
+    canvasContext.fillStyle = "white";
     canvasContext.beginPath();
     canvasContext.arc(ballX, ballY, 10, 0, Math.PI * 2, true);
     canvasContext.fill();
